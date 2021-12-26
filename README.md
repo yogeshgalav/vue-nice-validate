325afc2c97fe02b81cb0752ef23739ba51e2c816
DESKTOP-HG0S5P4\Yogesh Galav <yogesh@groony.onmicrosoft.com>
2021-12-26 22:59:15 +0530

    intital commit

--- README.md
+++ README.md
@@ -1,9 +1,100 @@
-# vue-nice-table
+# Vue Form Mixin
 
 ## Project setup
 ```
 npm install
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Usage
+### Basic Usage
+```
+import FormMixin from 'vue-form-mixin'
+
+export default {
+    mixins:[FormMixin]
+}
+```
+### Validating form fields
+```
+<input
+    name="field_name"
+    v-validate="'required|max:5'"
+>
+
+or pass object
+
+<input
+    name="field_name"
+    v-validate="{required:true,max:5}"
+>
+
+
+Use formErrors Method with field_name as paramter to get error
+
+<span class="text-danger">{{ formErrors('field_name') }}</span>
+```
+### Get validation errors
+```
+use validateForm() method to validate all fields.
+It return a promise with a resolved value as true or false.
+
+methods:{
+    handleSubmit(){
+        this.validateForm().then(result=>{
+            if(result){
+                \\validation successfull
+            }else{
+                \\validation failed
+            }
+        });
+    }
+}
+
+use this.form_errors to get all errors
+```
+
+### Validate multiple forms
+```
+To validate on only a single form use attribute validationScope
+
+<form validationScope="form_name">
+    <input
+        name="field_name"
+        v-validate="'required'"
+    >
+    <span class="text-danger">{{ formErrors('form_name.field_name') }}</span>
+</form>
+
+methods:{
+    handleSubmit(){
+        this.validateForm('form_name').then(result=>{
+            if(result){
+                \\validation successfull
+            }else{
+                \\validation failed
+            }
+        });
+    }
+}
+```
+### Use dynamic input names
+```
+Underscores will be automatically removed from field names in error message
+
+For dynamic input fields use field name and hashtag
+<input
+    :name="'field_name#'+123"
+    v-validate="'required'"
+>
+<span class="text-danger">{{ formErrors('field_name#'+123) }}</span>
+
+and error will be show as "field name is required."
+```
+### Use only number inside input field
+```
+<input
+    :name="'field_name'"
+    @input="onlyNumber"
+    v-validate="'required'"
+>
+```
--- src/form-mixin.js
+++ src/form-mixin.js
@@ -34,14 +34,16 @@ const FormMixin = {
 		};
 	},
 	methods:{
-		validateForm(){
+		validateForm(FormName=''){
 			//run validation and add error to this.form_errors
 			return new Promise((resolve, reject) => {
 				this.form_errors = [];
 				form_fields.forEach((form_field)=>{ 
+					if(FormName && form_field.form_name!==FormName) continue;
+					
 					for (const [key, value] of Object.entries(form_field.rules)) {
 						let field_value = document.getElementsByName(form_field.field_name)[0].value;
-						console.log(field_value,runValidation[key](field_value));
+
 						if(!runValidation[key](field_value)){
 							this.form_errors.push({
 								'field_name':form_field.field_name,
@@ -78,68 +80,17 @@ const FormMixin = {
 			}
 			let field_name = field_error.field_name;
 			//remove string between brackets and remove underscore
-			field_name=field_name.replace(/_/g, ' ').replace(/ *\([^)]*\) */g, ' ');
-			return  this.$t('validation.'+field_error.rule_name,{'attribute': field_name});
-		},
-		convertTime12(time24){
-			var ts = time24;
-			var H = +ts.substr(0, 2);
-			var h = (H % 12) || 12;
-			h = (h < 10)?('0'+h):h;  // leading 0 at the left for 1 digit hours
-			var ampm = H < 12 ? ' am' : ' pm';
-			ts = h + ts.substr(2, 3) + ampm;
-			return ts;
+			field_name=field_name.replace(/_/g, ' ').split('#')[0];
+			if(this.$t){
+				return  this.$t('validation.'+field_error.rule_name,{'attribute': field_name});
+			}
+			return 
 		},
 		onlyNumber ($event) {
 			let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
 			if ((keyCode < 48 || keyCode > 57) && keyCode !== 190) { // 46 is dot
 				$event.preventDefault();
 			}
-		},
-		processParticipantEntriesForGraph(journal_entries, commitmentDuration){
-			if (journal_entries.length === 0 ) {
-				return [];
-			}
-			let startDate = '';
-			let journalArray = [];
-			let journal_entries_rating=[];
-			journalArray = journal_entries.map(each => ({[each.action_date]: each.rating}));
-			startDate = this.$moment(journal_entries[0].action_date).format('YYYY-MM-DD');
-			let journalKey = 0;
-			for(let kk=0; kk <= commitmentDuration; kk++) {
-				const searchDate = journalArray.some((each) => each.hasOwnProperty(startDate));
-				if (searchDate) {
-					journal_entries_rating.push(journalArray[journalKey][startDate]);
-					startDate = this.$moment(startDate).add('1', 'd').format('YYYY-MM-DD');
-					journalKey = journalKey + 1;
-				} else {
-					journal_entries_rating.push(null);
-					startDate = this.$moment(startDate).add('1', 'd').format('YYYY-MM-DD');
-				}
-			}
-			return journal_entries_rating;
-		},
-		processBuddyEntriesForGraph(journal_entries, commitment, commitmentDuration) {
-			if (journal_entries.length === 0 ) {
-				return [];
-			}
-			let journalArray = [];
-			let journal_entries_rating=[];
-			const commitmentDate = this.dateInLocale(commitment.created_at).format('YYYY-MM-DD');
-			journalArray = journal_entries.sort((a, b) => {
-				return a.commitmentDay - b.commitmentDay;
-			}).map(each => ({[this.daysDiff(each.action_date, commitmentDate)]: each.rating}));
-			let journalKey = 0;
-			for(let kk=0; kk <= commitmentDuration; kk++) {
-				const searchDate = journalArray[journalKey] ? Object.keys(journalArray[journalKey])[0] : 0;
-				if (kk ===  parseInt(searchDate, 10)) {
-					journal_entries_rating.push(Object.values(journalArray[journalKey])[0]);
-					journalKey++;
-				} else {
-					journal_entries_rating.push(null);
-				}
-			}
-			return journal_entries_rating;
 		},
 	}
 };
