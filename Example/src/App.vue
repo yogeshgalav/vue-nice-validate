36d3af1bf3b78a98cd68b41af4bb22607a64d806
monti09 <gautamgjhaj@gmail.com>
2022-05-31 01:17:05 +0530

    added example-validate

---
+++ Example/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+<div id="app">
+  <form @submit.prevent="onEnter" form>
+  <input
+                        v-validate="'required'"
+                        name="chatroom"
+                        class="form-control"
+                        placeholder="write Chatroom Name here"
+                       
+                        
+                      >
+                      
+                      </form>
+  <!-- <form validationScope="newform">
+  <input
+  name="field_name"
+  :v-validate="required" 
+  placeholder="edit me">
+  <button v-on:click="handleSubmit()">button</button>
+  </form> -->
+</div>
+</template>
+
+
+<script>
+import  FormMixin   from '../../src/VueNiceValidate.vue'
+export default {
+  mixins:[FormMixin ],
+  name: 'App',
+  components: {
+  },
+  methods: {
+     onEnter(){
+        this.validateForm('field_name').then(result=>{
+            if(result){
+                console.log('asd');
+            }else{
+                console.log('asdd');
+            }
+        });
+    }
+  },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ Example/src/components/FirstMessage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="data">
+        <h2>{{data}}</h2>
+    </div>
+    
+</template>
+<script>
+export default {
+    name: 'FirstMessage',
+    props: {
+        data: String
+    }
+}
+</script>
+
+<style lang="scss">
+// h1{
+//     font-size: 2rem;
+//     font-weight: bold;
+//     color: #0062c5;
+// }
+h2{
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: rgb(220, 20, 133);
+}
+</style>
