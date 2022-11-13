e70e6922be68448644145aa1ec366fe0f9af5e1f
Yogesh Galav <yogesh@groonyindia.in>
2022-11-13 09:49:46 +0530

    vue3 version created

--- Example/src/App.vue
+++ Example/src/App.vue
@@ -1,61 +1,59 @@
 <template>
-<div id="app">
-    <h3>Validation</h3>
-  <form @submit.prevent="onEnter" validationScope=newform>
-  <input id="input"
-  name="field_name"
-  v-validate="'required'" 
-  placeholder="Text">
-  <br/>
-  <button v-on:click="onEnter" id="button">button</button>
-  <br/>
-  <label id="text">click button to check if input is valid or not</label>
-  </form>
-</div>
-</template>
-
-
-<script>
-import  FormMixin   from 'vue-nice-validate';
-export default {
-  mixins:[FormMixin ],
-  name: 'App',
-  methods: {
-     onEnter(){ 
-      var text = document.getElementById('text');
-
-        this.validateForm('newform').then(result=>{
-            if(result){
-                text.innerHTML='Its valid';
-                text.style.color='#008000';
-            }else{
-                text.innerHTML='Its not valid';
-                text.style.color='#ff0000';
-            }
-        });
-    }
-  },
-};
-
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-#input{
-  padding:10px;
-}
-#button{
-  margin:10px;
-}
-</style>
+  <div>
+      <h3>Validation</h3>
+    <form @submit.prevent="onEnter" validationScope=newform>
+    <input id="input"
+    name="field_name"
+    v-validate="'required'" 
+    placeholder="Text">
+    <br/>
+    <button v-on:click="onEnter" id="button">button</button>
+    <br/>
+    <label id="text">click button to check if input is valid or not</label>
+    </form>
+  </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    methods: {
+       onEnter(){ 
+        var text = document.getElementById('text');
+          console.log('wew',this.$validator);
+          this.$validator.validateForm('newform').then(result=>{
+              if(result){
+                  text.innerHTML='Its valid';
+                  text.style.color='#008000';
+              }else{
+                  text.innerHTML='Its not valid';
+                  text.style.color='#ff0000';
+              }
+          });
+      }
+    },
+  };
+  
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  #input{
+    padding:10px;
+  }
+  #button{
+    margin:10px;
+  }
+  </style>
+  
--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
-import VueNiceValidate from "./VueNiceValidate.js";
+import validator from "./VueNiceValidate.js";
 
-function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Vue.mixin(VueNiceValidate);
+function setFormFieldData(field, rules, formName) {
+  let validation_rules = {};
+  if (typeof rules === "string") {
+    rules.split("|").map((node) => {
+      let ru = node.split(":");
+      validation_rules[ru[0]] = ru[1] ? ru[1] : true;
+    });
+  } else if (typeof rules === "object") {
+    validation_rules = Object.assign({}, rules);
+  } else {
+    return false;
+  }
+  validator.form_fields.push({
+    field_name: field,
+    rules: validation_rules,
+    form_name: formName ? formName : ""
+  });
+  return true;
 }
 
-const plugin = {
-  install
+export default {
+  install: (app, options) => {
+    app.config.globalProperties.$validator = validator;
+
+    app.directive("validate", {
+      inserted: function (el, binding) {
+        let form = el.closest("form");
+        setFormFieldData(
+          el.getAttribute("name"),
+          binding.value,
+          form ? form.getAttribute("validationScope") : ""
+        );
+      }
+    });
+  }
 };
-
-let GlobalVue = null;
-if (typeof window !== "undefined") {
-  GlobalVue = window.Vue;
-} else if (typeof global !== "undefined") {
-  GlobalVue = global.vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
-VueNiceValidate.install = install;
-
-export default VueNiceValidate;
