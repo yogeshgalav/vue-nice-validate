e60086004df576da05304328555d99eb88217d35
AzureAD\MouneshPattar <Mounesh@groonyindia.in>
2023-09-17 12:57:12 +0530

    validate test

--- Example/src/App.vue
+++ Example/src/App.vue
@@ -4,30 +4,30 @@
             <div>
                 <div class="border border-indigo-600 p-4">
                     <h3>Basic Validation</h3>
-                    <form @submit.prevent="validateForm1" validationScope="form1">
+                    <form @submit.prevent="validateForm1">
                         <div class="mb-6">
                             <label for="full_name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                            <input type="text" id="full_name" v-model="form1.full_name" v-validate="'required'"
+                            <input type="text" id="form1.full_name" v-model="form1.full_name" v-validate="'required'"
                                 class="input-field">
-                                <span class="text-red-500">{{ fieldErrors["form1.full_name"] }}</span>
+                                <span class="text-red-500">{{ formErrors["form1.full_name"] }}</span>
                         </div>
 
                         <div class="mb-6">
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="text" id="email" v-model="form1.email" v-validate="'required'"
+                            <input type="text" id="form1.email" v-model="form1.email" v-validate="'required'"
                                 class="input-field">
-                                <span class="text-red-500">{{ fieldErrors["form1.email"] }}</span>
+                                <span class="text-red-500">{{ formErrors["form1.email"] }}</span>
 
                         </div>
 
                         <div class="mb-6">
                             <label for="age"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
-                            <input type="text" id="age" v-model="form1.age" v-validate="'required'"
+                            <input type="text" id="form1.age" v-model="form1.age" v-validate="'required'"
                                 class="input-field">
-                                <span class="text-red-500">{{ fieldErrors["form1.age"] }}</span>
+                                <span class="text-red-500">{{ formErrors["form1.age"] }}</span>
                         </div>
 
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
@@ -35,11 +35,24 @@
                 </div>
             </div>
             <!-- ... -->
+            <!-- <div>
+                <h3>Validation</h3>
+                <div class="border border-indigo-600">
+
+                    <form @submit.prevent="" validationScope=newform>
+                        <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
+                        <br />
+                        <button v-on:click="" id="button">button</button>
+                        <br />
+                        <label id="text">click button to check if input is valid or not</label>
+                    </form>
+                </div>
+            </div>
             <div>
                 <h3>Validation</h3>
                 <div class="border border-indigo-600">
 
-                    <form @submit.prevent="onEnter" validationScope=newform>
+                    <form @submit.prevent="" validationScope=newform>
                         <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
                         <br />
                         <button v-on:click="onEnter" id="button">button</button>
@@ -52,10 +65,10 @@
                 <h3>Validation</h3>
                 <div class="border border-indigo-600">
 
-                    <form @submit.prevent="onEnter" validationScope=newform>
+                    <form @submit.prevent="" validationScope=newform>
                         <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
                         <br />
-                        <button v-on:click="onEnter" id="button">button</button>
+                        <button v-on:click="" id="button">button</button>
                         <br />
                         <label id="text">click button to check if input is valid or not</label>
                     </form>
@@ -65,23 +78,10 @@
                 <h3>Validation</h3>
                 <div class="border border-indigo-600">
 
-                    <form @submit.prevent="onEnter" validationScope=newform>
+                    <form @submit.prevent="" validationScope=newform>
                         <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
                         <br />
-                        <button v-on:click="onEnter" id="button">button</button>
-                        <br />
-                        <label id="text">click button to check if input is valid or not</label>
-                    </form>
-                </div>
-            </div>
-            <div>
-                <h3>Validation</h3>
-                <div class="border border-indigo-600">
-
-                    <form @submit.prevent="onEnter" validationScope=newform>
-                        <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
-                        <br />
-                        <button v-on:click="onEnter" id="button">button</button>
+                        <button v-on:click="" id="button">button</button>
                         <br />
                         <label id="text">click button to check if input is valid or not</label>
                     </form>
@@ -91,15 +91,15 @@
                 <div class="border border-indigo-600">
                     <h3>Validation</h3>
 
-                    <form @submit.prevent="onEnter" validationScope=newform>
+                    <form @submit.prevent="" validationScope=newform>
                         <input id="input" name="field_name" v-validate="'required'" placeholder="Text">
                         <br />
-                        <button v-on:click="onEnter" id="button">button</button>
+                        <button v-on:click="" id="button">button</button>
                         <br />
                         <label id="text">click button to check if input is valid or not</label>
                     </form>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
--- src/VueNiceValidate.js
+++ src/VueNiceValidate.js
@@ -1,8 +1,8 @@
 import validationRules from './validationRules.js';
 import validationMessages from './validationMessages.js';
 
-const form_fields = [];
-function setFormFieldData(fieldName, rules, formName) {
+const input_fields = [];
+function setInputFieldData(fieldId, fieldName='', rules, touch) {
 	let validation_rules = {};
 	if (typeof rules === "string") 
 		rules.split("|").map((node) => {
@@ -14,55 +14,57 @@ function setFormFieldData(fieldName, rules, formName) {
 	else 
 		return false;
 
-
-	const already_present_field = form_fields.find(el=>el.field_name===fieldName && el.formName===formName);
-	if(already_present_field){
-		already_present_field.rules = validation_rules;
-		return true;
-	}
-
-	form_fields.push({
+	let new_field = {
+		field_id: fieldId,
 		field_name: fieldName,
 		rules: validation_rules,
-		form_name: formName,
-	});
+	};
+	const already_present_field = input_fields.find(el=>el.field_id===fieldId);
+	if(already_present_field){
+		already_present_field.rules = validation_rules;
+	} else {
+		input_fields.push(new_field);
+	}
+	
+	if(touch){
+		runValidation([new_field]);
+	}
 	return true;
 }
 
 export default {
 	directives: {
 		validate:{
-			inserted: function (el,binding) {
-				let form = el.closest('form');
-				setFormFieldData(el.id, binding.value, (form ? form.getAttribute('validationScope') : ''));
-			}
+			inserted: function(el, binding) {
+				let field = el.tagName.toLowerCase() == 'input' ? el : el.getElementsByTagName('input')[0];
+				if(!field){
+					console.error('Input field not found.')
+				}
+
+				let field_name = field.name || field.id.replace(/_/g, ' ');
+				if(field_name.includes('.')) field_name = field_name.split('.').slice(-1).pop();
+
+				setInputFieldData(field.id, field_name, binding.value, binding.arg == 'touch');
+			},
 		}
 	},
 	data(){
 		return {
-			form_errors: [],
+			field_errors: [],
 		};
 	},
 	computed:{
-		fieldErrors(){
-			let field_errors = [];
-			this.form_errors.forEach(el=>{
-				let key = '';
-				if(el.form_name){
-					key = el.form_name+'.'+el.field_name
-				}else{
-					key = el.field_name 
-				}
-
-				let field_name = el.field_name;
-				field_name=field_name.replace(/_/g, ' ').split('#')[0];
-	
+		formErrors(){
+			let errors = [];
+			// console.log(this.field_errors);
+			this.field_errors.forEach(el=>{
+				let key = el.field_id;
 				
 				const val = validationMessages[el.rule_name]
-					.replace(':attribute', field_name)
+					.replace(':attribute', el.field_name)
 					.replace(':param', el.rule_param);
 				// let val = validationMessages[el.rule_name]
-				// 	.replace(':attribute', field_name);
+				// 	.replace(':attribute', field_id);
 
 				// el.rule_param.split(',').forEach((el2,in2)=>{
 				// 	val.replace(':param'+(in2 ? in2 : ''), el2);
@@ -70,95 +72,98 @@ export default {
 				// if(typeof this.$t === 'function'){
 				// 	return this.$t(validationMessages[el.rule_name],el);
 				// }
-				field_errors[key] = val;
+				errors[key] = val;
 			});
+			// console.log("errors",errors);
 			
-			return field_errors;
-		}
+			return errors;
+		},
 	},
+
 	methods:{
 		runValidation(to_be_validated_fields){
-			//run validation and add error to this.form_errors
+			//run validation and add error to this.field_errors
 			return new Promise((resolve, reject) => {
-				this.form_errors.length = 0;
+				this.field_errors.length = 0;
+				console.log(this.field_errors);
 				try{
 					to_be_validated_fields.forEach((field)=>{
 						for (const [rule_name, rule_parameter] of Object.entries(field.rules)) {
-							const field_element = document.getElementById(field.field_name) || document.querySelector('[validation-id="'+field.field_name+'"]');
+							const field_element = document.getElementById(field.field_id);
 
 							//continue if field not found during validation
 							if(!field_element) {
-								console.error('Field with id #'+field.field_name+' not found for validation.');
+								console.error('Field with id #'+field.field_id+' not found for validation.');
 								continue;
 							}
 							if(!validationRules[rule_name]){
-								console.error('Validation rule "'+rule_name+'" not found for field with id #'+field.field_name+'.');
+								console.error('Validation rule "'+rule_name+'" not found for field with id #'+field.field_id+'.');
 								continue;
 							}
 
-							const field_value = field_element.value || field_element.getAttribute('validation-value') || '';
-							const form_error = {
+							const field_value = field_element.value || '';
+							const field_error = {
+								'field_id':field.field_id,
 								'field_name':field.field_name,
 								'rule_name':rule_name,
 								'rule_param':rule_parameter,
-								'form_name':field.form_name,
 							};
-
 							if(!validationRules[rule_name](field_value, rule_parameter)){
-								this.form_errors.push(form_error);
-							}else{
-								this.form_errors.splice(this.form_errors.indexOf(form_error), 1);
+								this.field_errors.push(field_error);
+							} else {
+								this.field_errors.splice(this.field_errors.indexOf(field_error), 1)
 							}
-								
-
 						}
 					});
 				}catch(e){
 					reject(e);
 				}
-				if(this.form_errors.length)
+				if(this.field_errors.length)
 					resolve(false);
       
 				resolve(true);
 			});
 		},
+
 		
-		validateDirective(el, binding) {
-			const form = el.closest("form");
-			this.setFormFieldData(
-				el.getAttribute("name"),
-				binding.value,
-				form ? form.getAttribute("validationScope") : "",
-			);
-		},
+		// validateDirective(el, binding) {
+		// 	let field = el.tagName.toLowerCase() == 'input' ? el : el.getElementsByTagName('input')[0];
+		// 	if(!field){
+		// 		console.error('Input field not found.')
+		// 	}
+		// 	setInputFieldData(field.id, field.name, binding.value, binding.arg == 'touch');
+		// },
 
 		validateForm(FormName=''){
-			const to_be_validated_fields = form_fields
-				.filter(form_field=>{
-					if(FormName && form_field.form_name!==FormName) return false;
+
+			const to_be_validated_fields = input_fields
+				.filter(field=>{
+					if(FormName && !field.field_id.includes(FormName+'.')) return false;
 					return true;
 				});
 			return this.runValidation(to_be_validated_fields);
 		},
 
 		validateInputs(validate_fields=[]){
-			const to_be_validated_fields = form_fields
-				.filter(el=>validate_fields.includes(el.field_name));
+			const to_be_validated_fields = input_fields
+				.filter(el=>validate_fields.includes(el.field_id));
 			return this.runValidation(to_be_validated_fields);
 		},
 
 		validateInput(validate_field=''){
-			const to_be_validated_fields = form_fields
-				.filter(el=>el.field_name===validate_field);
+			const to_be_validated_fields = input_fields
+				.filter(el=>el.field_id===validate_field);
 			return this.runValidation(to_be_validated_fields);
 		},
 
-		addField(field,validation_rules,formName){
-			if(form_fields.some(el=>el.field_name===field)) return false;
-			form_fields.push({
-				'field_name':field,
+		addField(field_id,validation_rules,field_name=''){
+			//if already present don't add to input_fields bag
+			if(input_fields.some(el=>el.field_id===field_id)) return false;
+			//add to input_fields bag
+			input_fields.push({
+				'field_id':field_id,
+				'field_name':field_name,
 				'rules':validation_rules,
-				'form_name':formName ? formName : '',
 			});
 			return true;
 		},
